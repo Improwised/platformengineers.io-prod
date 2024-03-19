43037629a05730ea298255ded50dfdb3c5af239b
payalraviya <payalraviya@users.noreply.github.com>
2024-03-19 05:08:46 +0000

    deploy: Improwised/platformengineers.io@23eeac9764d3e0e0371232350827eb3ae1d5f18f

---
+++ _nuxt/979d874.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{499:function(n,t,e){"use strict";e.r(t);var r={asyncData:function(n){return(0,n.redirect)("/")}},c=e(46),component=Object(c.a)(r,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
+//# sourceMappingURL=979d874.js.map
